a1db1cd349980e88656fc87679acc1c84e052482
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-02-01 07:01:31 +0545

    todo app final

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles.css';
-
-/*
-  INSTRUCTIONS:
-  Create a "todo" app with the following criteria.
-    1. The user can add new todo items
-    2. The user can remove todo items
-*/
+function generateId() {
+  return '_' + Math.random().toString(36).substring(2, 9);
+}
 
 function Todo() {
-  return <div>Follow the instructions above.</div>;
+  const [todos, setTodos] = React.useState([]);
+  const [input, setInput] = React.useState('');
+
+  const handleSubmit = () => {
+    setTodos((todos) => todos.concat({ text: input, id: generateId() }));
+    setInput('');
+  };
+
+  const removeTodo = (id) =>
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={input}
+        placeholder='New Todo'
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <ul>
+        {todos.map(({ text, id }) => (
+          <li key={id}>
+            <span>{text} </span>
+            <button onClick={() => removeTodo(id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById('root');
