55b0c92a2fd3bdfc7f2399136ffa3d061973423e
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-02-01 06:19:13 +0545

    darkmode with class component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css';
 
-ReactDOM.render('hi', document.getElementById('root'));
+/*
+  INSTRUCTIONS:
+  Convert the code below from a Class component
+  using setState to a function component using 
+  the useState Hook.
+*/
+
+class Theme extends React.Component {
+  state = {
+    theme: 'light',
+  };
+
+  toDark = () => this.setState({ theme: 'dark' });
+  toLight = () => this.setState({ theme: 'light' });
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <div className={theme}>
+        {theme === 'light' ? (
+          <button onClick={this.toDark}>🔦</button>
+        ) : (
+          <button onClick={this.toLight}>💡</button>
+        )}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Theme />, rootElement);
