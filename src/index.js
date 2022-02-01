8ebbddca3409c1eab5c8bcd28db16a60e2091680
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-02-01 09:13:40 +0545

    show hide app final

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/*
-  Instructions:
-    Given the array of "posts", recreate the functionality for this app. 
-    By default, each post preview is cut off until the user clicks "Open". 
-    Only one post can be "Open" at a time.
-*/
+import './styles.css';
 
 function App({ posts }) {
-  return <div>{JSON.stringify(posts, null, 2)}</div>;
+  const [openIndex, setOpenIndex] = React.useState(0);
+
+  return (
+    <ul>
+      {posts.map(({ id, img, text }, index) => {
+        const isOpen = openIndex === index;
+
+        const textToShow = isOpen ? text : text.substring(0, 100) + '...';
+
+        return (
+          <li key={id} style={{ border: isOpen ? '1px solid black' : 'none' }}>
+            <img src={img} alt='Post avatar' />
+            <p>{textToShow}</p>
+            {!isOpen && (
+              <button onClick={() => setOpenIndex(index)}>Open</button>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 const rootElement = document.getElementById('root');
@@ -19,11 +33,12 @@ ReactDOM.render(
       {
         id: 0,
         img: 'https://res.cloudinary.com/gorakhjoshi-com/image/upload/v1643684133/gorakhjoshi.com/JS_k0akkh.png',
-        text: "With this course you are going to learn beyond just the basics. You won't just learn patterns, techniques and best practices. You are going to understand the 'why' of the toughest part of the language, to the point that when you get asked any question about Javascript in an interview or in a meeting, you will be able to explain concepts that would truly make people see that you are a senior javascript programmer. Whether you are a web developer, a React, Angular, Vue.js developer (frontend developer), or a Node.js backend developer, you will benefit from this course because Javascript is at the core of these professions.",
+        text: 'This project based live session will introduce you to all of the modern toolchain of a React developer in 2022. Along the way, we will build a massive e-commerce application similar to Shopify using React, Redux, React Hooks, React Suspense, React Router, GraphQL, Context API, Firebase, Redux-Saga, Stripe + more. This is going to be a full stack app (MERN stack), using Firebase.',
       },
       {
         id: 1,
-        img: 'This project based live session will introduce you to all of the modern toolchain of a React developer in 2022. Along the way, we will build a massive e-commerce application similar to Shopify using React, Redux, React Hooks, React Suspense, React Router, GraphQL, Context API, Firebase, Redux-Saga, Stripe + more. This is going to be a full stack app (MERN stack), using Firebase.',
+        img: 'https://res.cloudinary.com/gorakhjoshi-com/image/upload/v1643683670/gorakhjoshi.com/React_Hook_mjea3c.png',
+        text: 'This project based live session will introduce you to all of the modern toolchain of a React developer in 2022. Along the way, we will build a massive e-commerce application similar to Shopify using React, Redux, React Hooks, React Suspense, React Router, GraphQL, Context API, Firebase, Redux-Saga, React Testing Library + more. This is going to be a full stack app (MERN stack), using Firebase.',
       },
       {
         id: 2,
